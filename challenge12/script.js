0dd9c1a4b657444d899ea6879dc539a983ddd379
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-11-02 16:29:48 +0330

    fix: comment
    add comment to JS codes.

--- challenge12/script.js
+++ challenge12/script.js
@@ -1,3 +1,6 @@
+/**
+ * Store the math operators
+ */
 const mathOperators = ["+", "-", "*", "/"];
 
 /**
@@ -46,25 +49,29 @@ function operatorDiv(number1, number2) {
  * From getting number value from the user to displaying the final result to the user.
  */
 function calculatorHandler() {
+  // Get user inputs
   const number1 = document.getElementById("user-input-1").value;
   const mathOperator = document.getElementById("user-input-2").value;
   const number2 = document.getElementById("user-input-3").value;
 
+  // Validation user inputs (number 1)
   if (!isValidNumber(number1)) {
     return;
   }
-
+  // Validation user inputs (number 2)
   if (!isValidNumber(number2)) {
   }
-
+  // Validation user inputs (math operator)
   if (!mathOperators.includes(mathOperator)) {
     showResult("⚠️ Invalid Operator");
     return;
   }
 
+  // Convert string value to number
   const convertToNumber1 = Number(number1);
   const convertToNumber2 = Number(number2);
 
+  // Call the operator function
   switch (mathOperator) {
     case "+":
       operatorSum(convertToNumber1, convertToNumber2);
@@ -95,6 +102,13 @@ function showResult(text) {
   document.getElementById("result").textContent = text;
 }
 
+/**
+ * This management function checks whether the given value is a valid number under the following conditions
+ * - Not string
+ * - Not NaN
+ * - Not less and equal then 0
+ * - Not Float
+ */
 function isValidNumber(number) {
   if (isNaN(Number(number))) {
     showResult("⚠️ Not a Number");
@@ -107,6 +121,7 @@ function isValidNumber(number) {
   return true;
 }
 
+// Active submit listener
 document
   .getElementById("user-submit")
   .addEventListener("click", calculatorHandler);
