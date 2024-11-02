5cdd17059137beb3e9e71f2dd8af0d8bdb718686
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-11-02 16:48:56 +0330

    fix: comment
    add comment to JS codes.

--- challenge2/script.js
+++ challenge2/script.js
@@ -2,15 +2,19 @@ let gameNumber = 0;
 let gameGuessCounter = 0;
 
 function gameInit() {
+  // Set random number for "Game Number"
   gameNumber = Math.floor(Math.random() * 100) + 1;
 
+  // Set counter to 0
   gameGuessCounter = 0;
 }
 
+// this function manages the display of output to the user.
 function showResult(text) {
   alert(text);
 }
 
+// function checks whether the given value is a valid number under the following conditions
 function isValidNumber(number) {
   if (isNaN(Number(number))) {
     showResult("⚠️ Not a Number");
@@ -26,19 +30,26 @@ function isValidNumber(number) {
   return true;
 }
 
+// Game preparation
 gameInit();
 
 // Game Loop
 while (true) {
+  // Counting new guess
   gameGuessCounter += 1;
 
+  // Get user inputs
   const number = prompt("Guess the number?", 0);
+
+  // Validation user inputs
   if (!isValidNumber(number)) {
     continue;
   }
 
+  // Convert number to string
   const userNumber = Number(number);
 
+  // Check the user input
   if (userNumber > gameNumber) {
     showResult("📉: Choose lower number");
   } else if (userNumber < gameNumber) {
@@ -46,6 +57,7 @@ while (true) {
   } else if (userNumber === gameNumber) {
     showResult(`👑: Winner, Guesses: ${gameGuessCounter}`);
 
+    // Reset the game
     gameInit();
   }
 }
