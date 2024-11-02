4c1aee3efa7a428c7a96023cd5fe2387cc706769
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-11-02 16:55:26 +0330

    fix: comment
    add comment to JS codes.

--- challenge4/script.js
+++ challenge4/script.js
@@ -6,6 +6,13 @@ function showResult(text) {
   document.getElementById("result").textContent = text;
 }
 
+/**
+ * This management function checks whether the given value is a valid number under the following conditions
+ * - Not string
+ * - Not NaN
+ * - Not less and equal then 0
+ * - Not Float
+ */
 function isValidNumber(number) {
   if (isNaN(Number(number))) {
     showResult("⚠️ Not a Number");
@@ -18,21 +25,34 @@ function isValidNumber(number) {
   return true;
 }
 
+/**
+ * This function takes the temperature from Celsius and converts it to Fahrenheit
+ */
 function toFarenhaight() {
+  // Get user input
   const temp = document.getElementById("user-input").value;
 
+  // Convert temperature to farenhaight with formula
   const tempToFarenhaight = (temp * 9) / 5 + 32;
 
+  // show to result to user
   showResult(`temperature: ${tempToFarenhaight} f`);
 }
 
+/**
+ * This function takes the temperature from Fahrenheit and converts it to Celsius
+ */
 function toCelsius() {
+  // Get user input
   const temp = document.getElementById("user-input").value;
 
+  // Convert temperature to farenhaight with formula
   const tempToCelsius = ((temp - 32) * 5) / 9;
 
+  // show to result to user
   showResult(`temperature: ${tempToCelsius} c`);
 }
 
+// Active submit listener
 document.getElementById("user-f").addEventListener("click", toFarenhaight);
 document.getElementById("user-c").addEventListener("click", toCelsius);
