7d9b880441349be3cc6b436925e09f5715c491f5
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-11-02 16:59:10 +0330

    fix
    fix the JS bug

--- challenge11/script.js
+++ challenge11/script.js
@@ -2,30 +2,23 @@
 
 // ------------------------------
 
-// create variable
-let numberValue;
+// create variable 
+let numberValue 
 
-// Using a loop to get an integer value from the user
-// while (true){
-//     // get user input with prompt
-let getInputValue = Number(prompt("Enter the desired number"));
+//  get user input with prompt
+let getInputValue = prompt("Enter the desired number")
 
-//     // Checking the numeric value of the input with the condition
-//     if ( getInputValue === null || !Number.isInteger(Number(getInputValue)) ) {
-//         continue
-//     } else{
-//         numberValue = getInputValue
-//         break
-//     }
 
-// }
 
 // Checking whether it is even or odd
-if (getInputValue % 2 !== 0) {
-  // Check for odd
-  // document.body.innerHTML
-  alert("Your number is odd");
-} else {
-  // Check for even
-  document.body.innerHTML = `Your number is even`;
+if (getInputValue % 2 === 0 ){
+
+    // Check for even
+    document.body.innerHTML = `Your number is even`;
+    
+}else {
+
+    // Check for odd
+    document.body.innerHTML = `Your number is odd`;
+
 }
