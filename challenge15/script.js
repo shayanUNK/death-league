89a658d1b81c543ba87bc58f40e9febcd42b2e80
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-11-02 16:38:04 +0330

    fix: comment
    add comment to JS codes.

--- challenge15/script.js
+++ challenge15/script.js
@@ -1,3 +1,4 @@
+// define the available symbols in a list
 let symbol = [
   "!",
   "@",
@@ -20,20 +21,23 @@ let symbol = [
   "<",
   "|",
 ];
-
+// create varable
 let words = [];
 let counter = 0;
-
+// select DOM element
 let input = document.querySelector("#input");
 let submit = document.querySelector("#submit");
 let output = document.querySelector("#output");
 
+// add event for get user choice
+// add arrow function for check user data
 submit.addEventListener("click", () => {
   output.textContent = "";
   counter = 0;
   let inputVal = input.value;
   words = inputVal.split("");
   words.forEach((element) => {
+    // Examining the presence of symbols with includes's method
     if (symbol.includes(element)) {
       counter += 1;
     }
