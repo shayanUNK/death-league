a12c55568633bdd43053e85a7c168d0d9ebfedab
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-11-02 16:07:35 +0330

    fix: comment
    add comment to JS codes.

--- challenge7/script.js
+++ challenge7/script.js
@@ -1,10 +1,13 @@
+// select element
 const courantNumber = document.querySelector("#number");
 const increaseBtn = document.querySelector("#increase");
 const resetBtn = document.querySelector("#reset");
 const decreaseBtn = document.querySelector("#decrease");
 
+// create variable
 let newNum;
 
+// event and function to increase the number
 increaseBtn.addEventListener("click", function () {
   newNum = Number(courantNumber.innerHTML) + 1;
   courantNumber.innerHTML = newNum;
@@ -12,12 +15,15 @@ increaseBtn.addEventListener("click", function () {
   courantNumber.style.color = "green";
 });
 
+// event and function to decrease the number
 decreaseBtn.addEventListener("click", function () {
   newNum = Number(courantNumber.innerHTML) - 1;
   courantNumber.innerHTML = newNum;
+  // color for decrease
   courantNumber.style.color = "red";
 });
 
+// event and function to reset the number
 resetBtn.addEventListener("click", function () {
   courantNumber.innerHTML = 0;
   courantNumber.style.color = "yellow";
